c0376f663fe682a91b69ecbfdfddb916ca620275
Rubén Palomo Fontán <99551384+RubenPalomo@users.noreply.github.com>
2022-08-28 14:24:06 +0200

    Create change_email.py

---
+++ change_email.py
@@ -0,0 +1,10 @@
+import os
+
+counter = 0
+files = os.listdir('dir_name')
+for file in files:
+    f = open('dir_name/' + file, 'r')
+    txt = f.read()
+    f = open('dir_name/' + file, 'w')
+    f.write(txt.replace("example@adress.com", "my_email@gmail.com"))
+    f.close()
