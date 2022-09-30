44449bb5180476efb81f433d641444046498d6ee
Rubén Palomo Fontán <99551384+RubenPalomo@users.noreply.github.com>
2022-09-30 11:49:31 +0200

    Update change_email.py

--- change_email.py
+++ change_email.py
@@ -7,13 +7,13 @@ def change(dir_name):
         files = os.listdir()
     try:
         for file in files:
-            f = open(file, 'r')
+            f = open(file, 'r+')
             txt = f.read()
-            f = open(file, 'w')
             f.write(txt.replace("example@adress.com", "my_email@gmail.com"))
             f.close()
         print("Files Modified Successfully")
     except:
         print("Files could not be modified")
-      
+
+
 change('')
